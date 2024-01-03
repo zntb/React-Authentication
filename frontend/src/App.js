11ecd178380e4d0bb7b0d7b53de5c73a6a2c9e37
zntb <zenetibi@yahoo.com>
2024-01-03 10:57:07 +0200

    Add Route Protection

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -17,7 +17,7 @@ import AuthenticationPage, {
   action as authAction,
 } from './pages/Authentication';
 import { action as logoutAction } from './pages/Logout';
-import { tokenLoader } from './util/auth';
+import { checkAuthLoader, tokenLoader } from './util/auth';
 
 const router = createBrowserRouter([
   {
@@ -56,6 +56,7 @@ const router = createBrowserRouter([
                 path: 'edit',
                 element: <EditEventPage />,
                 action: manipulateEventAction,
+                loader: checkAuthLoader,
               },
             ],
           },
@@ -63,6 +64,7 @@ const router = createBrowserRouter([
             path: 'new',
             element: <NewEventPage />,
             action: manipulateEventAction,
+            loader: checkAuthLoader,
           },
         ],
       },
