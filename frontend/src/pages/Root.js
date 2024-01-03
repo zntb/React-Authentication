2c5609f58a960dab3b1f1a876cd912b4e6ee4cc0
zntb <zenetibi@yahoo.com>
2024-01-03 11:10:37 +0200

    Add Automatic Logout - not perfect!

--- frontend/src/pages/Root.js
+++ frontend/src/pages/Root.js
@@ -1,9 +1,23 @@
-import { Outlet, useNavigation } from 'react-router-dom';
+import { Outlet, useLoaderData, useSubmit } from 'react-router-dom';
 
 import MainNavigation from '../components/MainNavigation';
+import { useEffect } from 'react';
 
 function RootLayout() {
+  const token = useLoaderData();
+  const submit = useSubmit();
   // const navigation = useNavigation();
+  useEffect(() => {
+    if (!token) {
+      return;
+    }
+
+    const timeout = 1 * 60 * 60 * 1000; // 1 hour
+
+    setTimeout(() => {
+      submit(null, { action: '/logout', method: 'post' });
+    }, timeout);
+  }, [token, submit]);
 
   return (
     <>
